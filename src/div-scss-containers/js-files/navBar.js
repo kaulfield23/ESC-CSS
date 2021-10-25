7d552fe7c348b095a6bece16d5870596810a4a0e
Haeju Eom <kaulfield23@gmail.com>
2021-10-25 23:03:19 +0200

    fixing slide

--- src/div-scss-containers/js-files/navBar.js
+++ src/div-scss-containers/js-files/navBar.js
@@ -5,3 +5,26 @@ const navbar = document.querySelector(".navBar");
 hamburger.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
+
+let flickity;
+window.onload = () => {
+  initiateFlickity();
+};
+let windowWidth = window.innerWidth;
+if (windowWidth > 500) {
+  window.addEventListener("resize", initiateFlickity);
+}
+
+function initiateFlickity() {
+  if (sizeIsLessThan500px()) {
+    flickity = new Flickity(".main-carousel", {
+      prevNextButtons: false,
+      pageDots: false,
+    });
+  }
+}
+
+function sizeIsLessThan500px() {
+  const horizontalWidth = window.innerWidth;
+  return horizontalWidth < 500;
+}
