d192607445b2cfc63d4db024a61b35b84730391e
kaulfield23 <kaulfield23@gmail.com>
2021-10-28 16:45:31 +0200

    added galaxy fold size styles

---
+++ src/div-scss-containers/js-files/initiateFlickity.js
@@ -0,0 +1,27 @@
+let flickity;
+window.onload = () => {
+  initiateFlickity();
+};
+
+window.addEventListener("resize", initiateFlickity);
+
+function initiateFlickity() {
+  const mainCarousel = document.querySelector(".main-carousel");
+  if (sizeIsLessThan500px() && flickity === undefined) {
+    console.info("created");
+    mainCarousel.classList.remove("flex-me");
+    flickity = new Flickity(".main-carousel", {
+      prevNextButtons: false,
+      pageDots: false,
+    });
+  } else if (!sizeIsLessThan500px()) {
+    mainCarousel.classList.add("flex-me");
+    flickity?.destroy();
+    flickity = undefined;
+  }
+}
+
+function sizeIsLessThan500px() {
+  const horizontalWidth = window.innerWidth;
+  return horizontalWidth < 1130;
+}
--- src/div-scss-containers/js-files/navBar.js
+++ src/div-scss-containers/js-files/navBar.js
@@ -5,31 +5,3 @@ const navbar = document.querySelector(".navBar");
 hamburger.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
-
-let flickity;
-window.onload = () => {
-  initiateFlickity();
-};
-
-window.addEventListener("resize", initiateFlickity);
-
-function initiateFlickity() {
-  const mainCarousel = document.querySelector(".main-carousel");
-  if (sizeIsLessThan500px() && flickity === undefined) {
-    console.info("created");
-    mainCarousel.classList.remove("flex-me");
-    flickity = new Flickity(".main-carousel", {
-      prevNextButtons: false,
-      pageDots: false,
-    });
-  } else if (!sizeIsLessThan500px()) {
-    mainCarousel.classList.add("flex-me");
-    flickity?.destroy();
-    flickity = undefined;
-  }
-}
-
-function sizeIsLessThan500px() {
-  const horizontalWidth = window.innerWidth;
-  return horizontalWidth < 1130;
-}
