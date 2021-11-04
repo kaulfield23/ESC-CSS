766a95ccc3fd7b064a12301133c944270593379a
kaulfield23 <kaulfield23@gmail.com>
2021-11-04 19:53:37 +0100

    adds comments

--- src/js-files/navBar.js
+++ src/js-files/navBar.js
@@ -1,33 +1,52 @@
 const hamburger = document.querySelector(".hamburger");
-const menu = document.querySelector(".navBar-menu");
-const navbar = document.querySelector(".navBar");
 const body = document.querySelector("body");
+const navBarMenu = document.querySelector(".navBar-menu");
 const menuName = document.querySelectorAll(".menu-name");
-const closeButton = document.createElement("div");
-const navBackground = document.createElement("div");
 
 hamburger.addEventListener("click", () => {
-  navBackground.setAttribute("class", "navBackground");
-  menu.setAttribute("class", "phone-nav");
-  closeButton.setAttribute("class", "xIcon");
+  //create and set attribute to elements
+  const navBackground = createElement("div", "navBackground");
   body.appendChild(navBackground);
+  const closeButton = createElement("div", "close-button");
+
+  //add fade in animation to navbar background
   navBackground.animate([{ opacity: 0 }, { opacity: 1 }], {
-    duration: 500,
+    duration: 300,
     iterations: 1,
   });
 
+  //pop up navbar after 350ms
   setTimeout(() => {
     menuName.forEach((aName) => {
       aName.setAttribute("class", "aNameHover");
     });
-    navBackground.appendChild(menu);
+    //add close button when pop up nav appears
     closeButton.innerHTML = '<i class="fas fa-times"></i>';
-    menu.appendChild(closeButton);
-  }, 550);
+    navBarMenu.setAttribute("class", "pop-up-nav");
+    navBarMenu.appendChild(closeButton);
+    navBackground.appendChild(navBarMenu);
+  }, 350);
+
+  //remove created elements
+  closeButton.addEventListener("click", () => {
+    navBarMenu.removeChild(closeButton);
+    navBackground.removeChild(navBarMenu);
+    body.removeChild(navBackground);
+
+    //reset the navbar menu style for desktop version
+    const navBar = document.querySelector(".navBar");
+    navBarMenu.setAttribute("class", "navBar-menu");
+    navBar.appendChild(navBarMenu);
+    navBackground.removeAttribute("class");
+    menuName.forEach((aName) => {
+      aName.setAttribute("class", "menu-name");
+    });
+  });
 });
 
-closeButton.addEventListener("click", () => {
-  menu.removeChild(closeButton);
-  navBackground.removeChild(menu);
-  body.removeChild(navBackground);
-});
+//function for create element and set attribute
+function createElement(elementType, className) {
+  const element = document.createElement(elementType);
+  element.setAttribute("class", className);
+  return element;
+}
